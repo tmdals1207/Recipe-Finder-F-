95b8de14f39614b1102ca03a973e225311fcf210
홍승민 <ghdtmdalsdad@naver.com>
2024-08-09 17:31:29 +0900

    #2 백엔드와 통신 확인 및 프록시 설정 (#1)

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
+import path from 'path';
 
-const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000, // 포트를 3000으로 설정
+  },
+  proxy: {
+    '/api': {
+      target: 'http://localhost:8080', // 백엔드 서버 주소
+      changeOrigin: true, // CORS 문제를 해결하기 위해 필요
+      secure: false, // HTTPS가 아닌 경우 false로 설정
+      rewrite: (path) => path.replace(/^\/api/, ''), // API 경로 리라이트
+    },
   },
   plugins: [
     
