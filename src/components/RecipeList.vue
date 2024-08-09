7b5660787b7fad031e99b38acdd1d9d0fa81e923
tmdals1207 <ghdtmdalsdad@naver.com>
2024-08-09 17:30:10 +0900

    #2 백엔드와 통신 확인 및 프록시 설정

--- src/components/RecipeList.vue
+++ src/components/RecipeList.vue
@@ -1,39 +1,44 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      recipes: [],
+    };
+  },
+  created() {
+    this.fetchRecipes();
+  },
+  methods: {
+    async fetchRecipes() {
+      try {
+        // 프록시를 통해 백엔드로 요청
+        const response = await axios.get('http://localhost:8080/api/recipes/all');
+        this.recipes = response.data;
+        console.log("Recipes fetched successfully");
+      } catch (error) {
+        console.error('Error fetching recipes:', error);
+      }
+    },
+  },
+};
+</script>
+
 <template>
-    <div>
-      <h1>Recipe List</h1>
-      <ul>
-        <li v-for="recipe in recipes" :key="recipe.id">
-          {{ recipe.name }}
-        </li>
-      </ul>
-    </div>
-  </template>
+  <div>
+    <h1>Recipe List</h1>
+    <ul v-if="recipes.length">
+      <li v-for="recipe in recipes" :key="recipe.id">
+        {{ recipe.name }}
+      </li>
+    </ul>
+    <p v-else>No recipes found.</p>
+  </div>
+</template>
+
   
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        recipes: [],
-      };
-    },
-    created() {
-      this.fetchRecipes();
-    },
-    methods: {
-      async fetchRecipes() {
-        try {
-          // 프록시를 통해 백엔드로 요청
-          const response = await axios.get('/api/recipes');
-          this.recipes = response.data;
-        } catch (error) {
-          console.error('Error fetching recipes:', error);
-        }
-      },
-    },
-  };
-  </script>
+
   
   <style scoped>
   /* 스타일 설정 */
