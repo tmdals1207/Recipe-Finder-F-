bc9be2461c477bd74a74cdcef0399d3112fe3139
홍승민 <ghdtmdalsdad@naver.com>
2024-08-22 09:14:26 +0900

    헤더에 로그인, 로그아웃, 회원가입 추가

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,38 @@
 <template>
     <header>
       <h1>Header Component</h1>
+      <nav>
+    <ul>
+      <li><router-link to="/home">Home</router-link></li>
+      <li><router-link to="/profile">Profile</router-link></li>
+      <li><button @click="logout">Logout</button></li>
+    </ul>
+  </nav>
     </header>
   </template>
   
   <script>
+import axios from 'axios';
+
+
   export default {
+    methods: {
+    logout() {
+      console.log(this.$store.getters.token)
+      axios.post('http://localhost:8080/api/auth/logout', {}, {
+        headers: {
+          Authorization: `Bearer ${this.$store.getters.token}`
+        }
+      })
+      .then(() => {
+        this.$store.dispatch('logout');
+        this.$router.push('/login');
+      })
+      .catch(error => {
+        console.error('Logout failed:', error);
+      });
+    }
+  },
     name: 'Header',
   };
   </script>
