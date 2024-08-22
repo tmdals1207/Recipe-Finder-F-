5024981f41ea6a3d2202d4da12e36984987db5e2
홍승민 <ghdtmdalsdad@naver.com>
2024-08-22 09:20:20 +0900

    [Feat] Login시 토큰 저장하도록 변경 및 홈으로 리다이렉트

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,47 +1,66 @@
 <template>
-    <div>
-      <h1>Login</h1>
-      <form @submit.prevent="onSubmit">
-        <input type="email" v-model="email" placeholder="Email" required />
-        <input type="password" v-model="password" placeholder="Password" required />
-        <button type="submit">Login</button>
-      </form>
-      <button @click="loginWithOAuth('google')">Login with Google</button>
-      <button @click="loginWithOAuth('naver')">Login with Naver</button>
-      <button @click="loginWithOAuth('kakao')">Login with Kakao</button>
-      <button @click="$router.push('/register')">Go to Register</button>
-    </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
+  <div>
+    <h1>Login</h1>
+    <form @submit.prevent="onSubmit">
+      <input type="email" v-model="email" placeholder="Email" required />
+      <input type="password" v-model="password" placeholder="Password" required />
+      <button type="submit" :disabled="loading">Login</button>
+    </form>
+    <button @click="loginWithOAuth('google')">Login with Google</button>
+    <button @click="loginWithOAuth('naver')">Login with Naver</button>
+    <button @click="loginWithOAuth('kakao')">Login with Kakao</button>
+    <button @click="$router.push('/register')">Go to Register</button>
+    <p v-if="loginError" style="color: red;">{{ loginError }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
 import { mapActions } from 'vuex';
-  
-  export default {
-    data() {
-      return {
-        email: '',
-        password: '',
-      };
-    },
-    methods: {
-      ...mapActions(['login']),
-      async onSubmit() {
-        try {
-          await axios.post('http://localhost:8080/api/auth/login',{
-            email: this.email, 
-            password: this.password 
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      loginError: null,
+      loading: false, // 로딩 상태 추가
+    };
+  },
+  methods: {
+    ...mapActions(['setAuthData']),
+    async onSubmit() {
+      this.loading = true; // 로딩 시작
+      try {
+        const response = await axios.post('http://localhost:8080/api/auth/login', {
+          email: this.email,
+          password: this.password,
+        });
+
+        console.log("API 응답:", response.data);
+
+        if (response.data.user) {
+          // 로그인 액션을 호출하여 Vuex에 사용자와 토큰 저장
+          this.setAuthData({
+            token: response.data.token,
+            user: response.data.user
           });
+
+          console.log("로그인 성공, 홈 페이지로 리다이렉트합니다.");
           this.$router.push('/');
-        } catch (error) {
-          console.error('Login failed:', error);
+        } else {
+          console.error("사용자 정보가 없습니다.");
         }
-      },
-      loginWithOAuth(provider) {
-        console.log(provider+"로 로그인 시작!")
-        window.location.href = `http://localhost:8080/oauth2/authorization/${provider}`;
-      },
+      } catch (error) {
+        console.error('Login failed:', error);
+        this.loginError = '로그인 실패: 이메일 또는 비밀번호를 확인하세요.';
+      } finally {
+        this.loading = false; // 로딩 종료
+      }
     },
-  };
-  </script>
-  
+    loginWithOAuth(provider) {
+      window.location.href = `http://localhost:8080/oauth2/authorization/${provider}`;
+    },
+  },
+};
+</script>
