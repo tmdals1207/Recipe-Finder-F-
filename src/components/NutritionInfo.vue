a3e8e6453c5ad6b2a8b6f85184b7a9984462de8b
홍승민 <ghdtmdalsdad@naver.com>
2024-12-06 00:43:08 +0900

    [Fix] 영양정보 받아올 때 에러 수정 (미완성)

--- src/components/NutritionInfo.vue
+++ src/components/NutritionInfo.vue
@@ -24,6 +24,7 @@ export default {
   async mounted() {
     if (this.recipeUrl) {
       try {
+        console.info("영양정보 불러오기!")
         const response = await axios.get('http://localhost:8080/api/edamam/nutrition', {
           params: { recipeUrl: this.recipeUrl },
         });
